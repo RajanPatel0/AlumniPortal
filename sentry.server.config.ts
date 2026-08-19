58e4fbfac6c87a612c409f2337bb2780255d97b5
Roushan kr <kumar7roushanjha2005@gmail.com>
2026-08-19 10:08:24 +0530

    add Sentry for server breakdown

---
+++ sentry.server.config.ts
@@ -0,0 +1,30 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e38df8bca33823b1ecb648a7dbf09751@o4511933016768512.ingest.de.sentry.io/4511933024567376",
+
+  environment: process.env.NODE_ENV,
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampler: (samplingContext) => {
+    // Exclude health check route from transaction performance tracing
+    if (samplingContext.name?.includes("/api/health")) {
+      return 0.0;
+    }
+    return 0.1;
+  },
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+});
